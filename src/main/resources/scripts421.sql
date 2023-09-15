0e1ec7a6d1a3b3b69a35d37fe2796b72581926d8
igorbratus <brat.igor1992@gmail.com>
2023-09-15 03:02:43 -0400

    unit commit scripts исправил чуток

--- src/main/resources/scripts421.sql
+++ src/main/resources/scripts421.sql
@@ -5,4 +5,4 @@ alter  table student add constraint name_unique unique(name);
 
 alter table faculty add constraint name_color_unique unique(name, color);
 
-alter table student alter column age set default 20;s
+alter table student alter column age set default 20;
