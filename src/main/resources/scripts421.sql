4b11b22788c9ac0197963553de070b5f0ad591d5
igorbratus <brat.igor1992@gmail.com>
2023-09-15 02:59:46 -0400

    unit commit scripts

---
+++ src/main/resources/scripts421.sql
@@ -0,0 +1,8 @@
+alter table student add constraint age_constrain check (age > 18);
+
+alter table student  alter column name set not null;
+alter  table student add constraint name_unique unique(name);
+
+alter table faculty add constraint name_color_unique unique(name, color);
+
+alter table student alter column age set default 20;s
